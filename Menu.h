3ccb20402e7926ff719933689f3cf1dfbf6f5ab1
darkboyd <shahar.amit111@gmail.com>
2016-06-29 20:38:26 +0300

    Add files via upload

--- Menu.h
+++ Menu.h
@@ -16,13 +16,14 @@ using namespace std;
 class Menu {
 private:
     bool inSystem;
+    int choice;
     string location;
+    ofstream myFile;
 public:
     void setInSystem(bool inSystem);
     Menu();
     void program();
     void printMenu();
-    ~Menu();
 };
 
 
--- Object.h
+++ Object.h
@@ -5,16 +5,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+
 
 using namespace std;
 
 class Object{
 public:
     string name;
+    virtual void printToFile(ofstream &os, int space);
     virtual ~Object();
     virtual void setName(string txt)  ;
     virtual string getName();
-    virtual void showContent(int tabs);
+    virtual void showContent(int space);
     virtual void deleteObj();
     virtual Object* makeCopy();
     virtual vector<Object*> getContent();
